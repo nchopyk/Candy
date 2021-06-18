838ff4829f52d2da5c7541b7b06f78788a9a73e5
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2021-06-18 20:52:44 +0300

    Server auth

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const authRouter = require('./routers/auth-router');
+const errorMiddleware = require('./middlewares/error-middleware');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', authRouter);
+app.use(errorMiddleware);
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}/`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
---
+++ server/routers/auth-router.js
@@ -0,0 +1,18 @@
+const Router = require('express').Router;
+const authController = require('../controllers/auth-controller');
+const router = new Router();
+const { body } = require('express-validator');
+const authMiddleware = require('../middlewares/auth-middleware');
+
+router.post('/registration',
+    body('email').isEmail(),
+    body('password').isLength({ min: 3, max: 32 }),
+    authController.registration);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.get('/activate/:link', authController.activate);
+router.get('/refresh', authController.refresh);
+router.get('/users', authMiddleware, authController.getUsers);
+
+module.exports = router;
+
